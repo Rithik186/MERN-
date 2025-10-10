f6d241cab6e1e4a951fad4cebf5e35185739dfc4
Rithik <rithikkannaa@gmail.com>
2025-10-10 12:18:28 +0530

    function

--- FRONT END/script.js
+++ FRONT END/script.js
@@ -1,4 +1,6 @@
-let a=0
-let b=a
-a=10
-console.log(a)
+function sum(a,b){
+    console.log(a+b);
+}
+let a = Number(prompt("Enter first number:"));
+let b = Number(prompt("Enter second number:"));
+sum(a,b);  
