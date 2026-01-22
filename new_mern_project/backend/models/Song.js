d5b430fd84bd9fde48a169caa9ba635495785538
Rithik <rithikkannaa@gmail.com>
2026-01-22 14:45:05 +0530

    updated packagejson

--- new_mern_project/backend/models/Song.js
+++ new_mern_project/backend/models/Song.js
@@ -2,8 +2,9 @@ const { default: mongoose } = require("mongoose")
 const userSchema = mongoose.Schema({
     song: {
         type: String,
-    }
+    },
     singer: {
-    
+        type: String
     }
-})
+});
+module.exports=mongoose.model('Song',Song)
